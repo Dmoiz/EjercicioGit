688607ecb98abf191848e6c07566985b9640fb96
Yoel <alexisyoel3030@gmail.com>
2022-09-23 20:21:55 +0200

    Funcion par impar

--- Practica1_ios/ViewController.swift
+++ Practica1_ios/ViewController.swift
@@ -14,10 +14,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         
-    
+    parImpar()
         
     }
+    
+    func parImpar(){
+        
+        let numeros = [1, 2, 3, 5, 8, 15, 31]
+        
+        for i in numeros{
+            
+            if i % 2 == 0 {
+                print("El numero \(i) es par")
+            }else{
+                print("El numero \(i) es impar")
+            }
+        }
 
 
 }
 
+}
