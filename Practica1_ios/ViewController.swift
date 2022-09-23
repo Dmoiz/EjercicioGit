c6084407d008e08769219221244579142a2517eb
Diego <dmorenoizq@gmail.com>
2022-09-23 19:40:00 +0200

    Initial Commit

---
+++ Practica1_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica1_ios
+//
+//  Created by Alumnos on 23/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
